ca5f5e518caf56b0482e1ef8e39873bcb0b533de
unknown <alejondrogarzon@gmail.com>
2021-03-09 11:44:33 -0500

    Alejandro

---
+++ f304/hojavida.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>curricullum vitae</title>
+</head>
+<body>
+<dl>
+<dt>nombre:</dt>
+<dd>Alejandro </dd>
+<dt>Apellido</dt>
+<dd>Montoya</dd>
+<dt>direccion</dt>
+<dd>calle45esur11</dd>
+<dt>Telefono</dt>
+<dd>5973690</dd>
+<dt>correo</dt>
+<dd>alejandromontoyagarzon@hotmail.com</dd>
+</dl>
+
+<ul>
+<li>indecap</li>
+<li>bachillerato</li>
+<li>primaria</li>
+</ul>
+<ol>
+<li>trabajo 1</li>
+</ol>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    
+    
+    
+    <title>Alejandro Montoya</title>
+</head>
+<body>
+<!-- Esto es un comentario,  -->
+<!-- titulos -- -->
+<h1> contenido en h1 </h1>
+<h2>contenido en h2</h2>
+<h3>contenido en h3</h3>
+<h4>contenido en h4</h4>
+<h5>contenido en h5</h5>
+<h6>contenido en h6</h6>
+<!-- textos -->
+<p>esto es parrafo</p>
+texto sin etiqueta
+<span>texto en span</span>
+<p> esto es parrafo esto es parrafo esto es parrafo esto es parrafo esto es parrafo esto es parrafo
+<!-- formatos -->
+-- <b> -> negrita, <i> -> italica, <small> -> tamaño texto = = >
+<p>Esto es un parrafo con una palabra en <b>negrita</b> </p>
+<p> Esto es un parrafo con una palabra en <i>italica</i> </p>
+<p> Esto es un parrafo con una palabra en <small>small</small> </p>
+<div>
+<span> texto 1</span> <hr width=''20%''> <span>texto 2</span>
+</div>
+<section>
+
+</section>
+<fieldset>
+<p>texto</p>
+<br> <br>
+</fieldset>
+<!--enlaces-->
+
+<a href="https://www.mclibre.org/consultar/htmlcss/html/html-etiquetas.html"></a>enlace a etiquetas html</a>
+<a href="https://www.mclibre.org/consultar/htmlcss/html/html-etiquetas.html"target''_blank'' enlace a etiquetas int
+<a href=''#''> enlace</a>
+<a> enlace sin atributos</a>
+
+</body>
+
+</html>
